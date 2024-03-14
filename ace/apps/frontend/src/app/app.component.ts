5f09021b631a813d30864dca66e6bd4f702a9559
DidoucheInSTI2D <gorimfanboy2002@gmail.com>
2024-03-14 03:04:22 +0100

    Created own adminNavbar component

--- ace/apps/frontend/src/app/app.component.ts
+++ ace/apps/frontend/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AdminNavbarComponent } from './admin-navbar/admin-navbar.component';
 
 @Component({
   standalone: true,
   imports: [
     RouterModule,
-    NavbarComponent
+    NavbarComponent,
+    AdminNavbarComponent
   ],
   selector: 'ace-root',
   templateUrl: './app.component.html',
