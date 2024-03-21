7ea3aff8735d3c03abce4f0da9a4b8b6cf8f228f
DidoucheInSTI2D <gorimfanboy2002@gmail.com>
2024-03-22 00:12:43 +0100

    Routing and Page for admin backoffice

---
+++ ace/apps/frontend/src/app/admin/review/review.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ace-review',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './review.component.html',
+  styleUrl: './review.component.css',
+})
+export class ReviewComponent {}
---
+++ ace/apps/frontend/src/app/admin/users/users.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ace-users',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './users.component.html',
+  styleUrl: './users.component.css',
+})
+export class UsersComponent {}
