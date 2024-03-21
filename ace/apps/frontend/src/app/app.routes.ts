245b06208ba8233be89003e3e7dd7228a90bc030
DidoucheInSTI2D <gorimfanboy2002@gmail.com>
2024-03-21 23:02:23 +0100

    Admin navbar in Navbar Repo

--- ace/apps/frontend/src/app/app.routes.ts
+++ ace/apps/frontend/src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Route, withDebugTracing } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 
 export const appRoutes: Route[] = [
     { path: 'home', component: HomeComponent },
-    { path: 'adminPanel', component: AdminPanelComponent }
 ];
