1a318cbbb0229024beaea926ad26ff48d63f6ec1
TheRealHoko <julien.zeybel@live.fr>
2024-03-26 17:59:18 +0100

    remove experimental properties endpoint

--- ace/apps/backend/src/app/app.module.ts
+++ ace/apps/backend/src/app/app.module.ts
@@ -1,6 +1,4 @@
 import { Logger, Module } from '@nestjs/common';
-import { PropertiesController } from './controllers/properties/properties.controller';
-import { PropertiesService } from './services/properties/properties.service';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { RolesModule } from './roles/roles.module';
@@ -11,7 +9,7 @@ import { RolesModule } from './roles/roles.module';
     DatabaseModule,
     RolesModule
   ],
-  controllers: [PropertiesController],
-  providers: [PropertiesService, Logger],
+  controllers: [],
+  providers: [Logger],
 })
 export class AppModule {}
