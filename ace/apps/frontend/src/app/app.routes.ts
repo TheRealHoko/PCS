16453ab783e046363e45b74de7f40e6bd1a14ae5
TheRealHoko <julien.zeybel@live.fr>
2024-03-27 03:18:43 +0100

    init register component & add titles

--- ace/apps/frontend/src/app/app.routes.ts
+++ ace/apps/frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Route, withDebugTracing } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 export const appRoutes: Route[] = [
     { path: 'home', component: HomeComponent },
-    { path: 'login', component: LoginComponent}
+    { path: 'login', component: LoginComponent, title: 'Login'},
+    { path: 'register', component: RegisterComponent, title: 'Register'},
 ];
