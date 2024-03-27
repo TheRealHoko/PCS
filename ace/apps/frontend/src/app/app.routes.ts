6005115cd82a911a8eb4374c0283e7960d2e08fa
TheRealHoko <julien.zeybel@live.fr>
2024-03-27 03:16:17 +0100

    init login component

--- ace/apps/frontend/src/app/app.routes.ts
+++ ace/apps/frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Route, withDebugTracing } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 export const appRoutes: Route[] = [
     { path: 'home', component: HomeComponent },
+    { path: 'login', component: LoginComponent}
 ];
