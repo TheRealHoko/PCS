dbd4f4c30908b6135d41bfc65d65422c1bb1d9c4
DidoucheInSTI2D <gorimfanboy2002@gmail.com>
2024-04-07 04:09:31 +0200

    Added TABLE for ADMIN PANEL + Added PROPERTIES endpoint (not finish)

--- ace/apps/frontend/src/app/admin/provider/provider.component.ts
+++ ace/apps/frontend/src/app/admin/provider/provider.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'ace-provider',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatTableModule
+  ],
   templateUrl: './provider.component.html',
   styleUrl: './provider.component.css',
 })
-export class ProviderComponent {}
+export class ProviderComponent {
+  dataSource = PROVIDER;
+  tableContenu: string[] = ['name', 'service', 'lieu'];
+  enAttenteValider = PROVIDER_WAITLIST;
+  tableContenuAttente: string[] = ['name', 'service', 'lieu']
+}
+
+export interface provider {
+  name: string;
+  service: string;
+  lieu: string;
+}
+
+export interface providerAttente {
+  name: string;
+  service: string;
+  lieu: string;
+}
+
+const PROVIDER : provider[] = [
+  {name: 'Mehdi', service: 'Nettoyage', lieu: 'Paris'},
+]
+
+const PROVIDER_WAITLIST: providerAttente[] = [
+  {name: 'Mehdi', service: 'Nettoyage', lieu: 'Paris'},
+];
+
