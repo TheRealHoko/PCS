2e8d7bbf668dd59d9972bde136305c8b719c31db
TheRealHoko <julien.zeybel@live.fr>
2024-04-08 23:53:40 +0200

    enhance table component & add edit delete button

--- ace/apps/frontend/src/app/admin/users/users.component.ts
+++ ace/apps/frontend/src/app/admin/users/users.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { UsersService } from '../../services/users.service';
 import { User } from '@ace/shared';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'ace-users',
@@ -10,20 +12,30 @@ import { User } from '@ace/shared';
   imports: [
     CommonModule,
     MatTableModule,
+    MatExpansionModule,
+    MatButtonModule
   ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css',
 })
 export class UsersComponent {
   users!: User[];
-  columns: string[] = [];
+  columns = [
+    { key: 'id', display: 'ID' },
+    { key: 'firstname', display: 'First Name' },
+    { key: 'lastname', display: 'Last Name' },
+    { key: 'email', display: 'Email' },
+    { key: 'phone', display: 'Phone' },
+    { key: 'roles', display: 'Roles' },
+    { key: 'addresses', display: 'Addresses' }
+  ];
+
+  displayedColumns: string[] = [...this.columns.map(c => c.key), 'actions'];
 
   constructor(private readonly usersService: UsersService) {
     this.usersService.getUsers().subscribe({
       next: (response) => {
         console.log(response);
-        this.columns = Object.keys(response[0]);
-        console.log(this.columns);
         this.users = response;
       },
       error: (err) => {
@@ -32,6 +44,17 @@ export class UsersComponent {
     });
   }
 
+  editRow(row: any) {
+    console.log(`editing ${row}`);
+  }
+
+  deleteRow(row: any) {
+    console.log(`deleting ${row}`);
+  }
+
+  isObjectType(value: any): boolean {
+    return typeof value === 'object' && value.length !== 0;
+  }
 }
 
 
